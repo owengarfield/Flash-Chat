e7ba1d2ccca6b96df4886e4ffe2988d709bdd527
Owen Richards <owengrichards@gmail.com>
2018-10-18 23:39:57 +0100

    Initial Commit

---
+++ Flash Chat/ChatViewController.swift
@@ -0,0 +1,147 @@
+//
+//  ViewController.swift
+//  Flash Chat
+//
+//  Created by Angela Yu on 29/08/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // Declare instance variables here
+
+    
+    // We've pre-linked the IBOutlets
+    @IBOutlet var heightConstraint: NSLayoutConstraint!
+    @IBOutlet var sendButton: UIButton!
+    @IBOutlet var messageTextfield: UITextField!
+    @IBOutlet var messageTableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //TODO: Set yourself as the delegate and datasource here:
+        
+        messageTableView.delegate = self
+        messageTableView.dataSource = self
+        
+        //TODO: Set yourself as the delegate of the text field here:
+
+        
+        
+        //TODO: Set the tapGesture here:
+        
+        
+
+        //TODO: Register your MessageCell.xib file here:
+
+        messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
+        
+    configureTableView()
+        
+    }
+
+    ///////////////////////////////////////////
+    
+    //MARK: - TableView DataSource Methods
+    
+    
+    
+    //TODO: Declare cellForRowAtIndexPath here:
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
+        
+        let messageArray = ["First message","second messarje roifrfiroireji jforijioJIO JIOJIEOJ IOJIoj ioj oij oj ioj oj ioj ioj oij wfoijijef owijf oij oijw ofiwge","third message"]
+        
+        cell.messageBody.text = messageArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //TODO: Declare numberOfRowsInSection here:
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    
+    //TODO: Declare tableViewTapped here:
+    
+    
+    
+    //TODO: Declare configureTableView here:
+    
+    func configureTableView (){
+        
+        messageTableView.rowHeight = UITableView.automaticDimension
+        messageTableView.estimatedRowHeight = 120.0
+        
+    }
+    
+    ///////////////////////////////////////////
+    
+    //MARK:- TextField Delegate Methods
+    
+    
+
+    
+    //TODO: Declare textFieldDidBeginEditing here:
+    
+    
+    
+    
+    //TODO: Declare textFieldDidEndEditing here:
+    
+
+    
+    ///////////////////////////////////////////
+    
+    
+    //MARK: - Send & Recieve from Firebase
+    
+    
+    
+    
+    
+    @IBAction func sendPressed(_ sender: AnyObject) {
+        
+        
+        //TODO: Send the message to Firebase and save it in our database
+        
+        
+    }
+    
+    //TODO: Create the retrieveMessages method here:
+    
+    
+
+    
+    
+    
+    @IBAction func logOutPressed(_ sender: AnyObject) {
+        
+        //TODO: Log out the user and send them back to WelcomeViewController
+        do{
+            
+        
+        try Auth.auth().signOut()
+            navigationController?.popViewController(animated: true)
+            
+        }
+        
+        catch{
+            print("error signing out")
+            
+        }
+        
+    }
+    
+
+
+}
